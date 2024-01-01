696535f14c4154168b398458029d2a71ce8f0553
Akhand Pratap Singh <akhandpratapsingh@gmail.com>
2024-01-01 21:47:35 +0530

    ALL change of SS_Logistic

---
+++ UltraLogistics/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UltraLogistics.Models;
+
+namespace UltraLogistics.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(MailSender ms)
+        {
+            string Subject = "Congratulation! ";
+            string body = "Now Your are a Customer of UltaLogicties.....! ";
+            ms.Send(ms.email, Subject, body);
+            Response.Write("<script>alert('Your record is is fill Sucessfull')</script>");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+        public ActionResult Services()
+        {
+
+            return View();
+        }
+        public ActionResult Price()
+        {
+            return View();
+        }
+        public ActionResult Features()
+        {
+            return View();
+        }
+        public ActionResult Quote()
+        {
+            return View();
+        }
+        public ActionResult Team()
+        {
+            return View();
+        }
+        public ActionResult Testimonial()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ UltraLogistics/Models/MailSender.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace UltraLogistics.Models
+{
+    public class MailSender
+    {
+        public string email { get; set; }
+        public bool Send(string email, string Subject, string message)
+        {
+            string sender = "akhandpratapsingh840@gmail.com";
+            string pass = "zpow lbje bemu doys";
+            var senderEmail = new MailAddress(sender);
+            var receiverEmail = new MailAddress(email, "Receiver");
+            var password = pass;
+            var sub = Subject;
+            var body = message;
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(senderEmail.Address, password)
+            };
+            using (var mess = new MailMessage(senderEmail, receiverEmail)
+            {
+                Subject = Subject,
+                Body = body,
+            })
+            {
+                smtp.Send(mess);
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
