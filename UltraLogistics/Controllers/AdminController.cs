936cd3e54ccbfc4eca9311617a053665e199eedf
Akhand Pratap Singh <akhandpratapsingh@gmail.com>
2024-01-06 01:03:46 +0530

    Admin Controller , IUD Method, Admin Master Pages , Views Page of Admin and Models

--- UltraLogistics/Controllers/AdminController.cs
+++ UltraLogistics/Controllers/AdminController.cs
@@ -1,25 +1,88 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UltraLogistics.Models;
 
 namespace UltraLogistics.Controllers
 {
     public class AdminController : Controller
     {
+        DBManager db = new DBManager();
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
+        [HttpGet]
         public ActionResult AddZipCode()
         {
+            DataTable dt = new DataTable();
+            SqlParameter[] parms =
+            {
+                new SqlParameter("@Action", 3)
+            };
+            dt = db.Proc_GetAllRecord("proc_Pincode", parms);
+            ViewBag.Record = dt;
             return View();
         }
+        [HttpPost]
+        public ActionResult AddZipCode(string DdlPincode)
+        {
+            bool res = false;
+            SqlParameter[] parm =
+            {
+                new SqlParameter("@Action",1),
+                new SqlParameter("@PinId",DdlPincode)
+            };
+            res = db.IUD("proc_Pincode", parm);
+            ViewBag.Mess = "ZipCode is Activate...";
+
+            DataTable dts = new DataTable();
+            SqlParameter[] parms =
+            {
+                new SqlParameter("@Action", 3)
+            };
+            dts = db.Proc_GetAllRecord("proc_Pincode", parms);
+            ViewBag.Record = dts;
+
+            return View();
+        }
+        [HttpGet]
         public ActionResult ViewZipCode()
         {
+            DataTable dt = new DataTable();
+            SqlParameter[] parms =
+            {
+                new SqlParameter("@Action", 4)
+            };
+            dt = db.Proc_GetAllRecord("proc_Pincode", parms);
+            ViewBag.Record = dt;
             return View();
+        }
+        public ActionResult ViewZipCodes(int PinId)
+        {
+            bool res = false;
+            SqlParameter[] parm =
+            {
+                new SqlParameter("@PinId",PinId),
+                new SqlParameter("@Action",2)
+            };
+            res = db.IUD("proc_Pincode", parm);
+            Response.Write("<script>alert('ZipCodes is DeActivate')</script>");
+
+            DataTable dt = new DataTable();
+            SqlParameter[] parms =
+            {
+                new SqlParameter("@Action", 4)
+            };
+            dt = db.Proc_GetAllRecord("proc_Pincode", parms);
+            ViewBag.Record = dt;
+
+            return View("ViewZipCode");
         }
         public ActionResult AddBranch()
         {
---
+++ UltraLogistics/Models/DBManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace UltraLogistics.Models
+{
+    public class DBManager
+    {
+        string con = "Data Source=APS-LAMBERDAR\\SQLEXPRESS01;Initial Catalog=Logistic;Integrated Security=True";
+        private SqlConnection connection;
+
+        public DBManager()
+        {
+            connection = new SqlConnection(con);
+        }
+
+        public bool IUD(string Proc,SqlParameter[] prams)
+        {
+            bool res = false;
+            try
+            {
+                SqlCommand sql = new SqlCommand(Proc, connection);
+                sql.CommandType = CommandType.StoredProcedure;
+                sql.Parameters.AddRange(prams);
+                connection.Open();
+                int n= sql.ExecuteNonQuery();
+                connection.Close();
+                res = n > 0 ? true : false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return res;
+        }
+
+        public DataTable Proc_GetAllRecord(string Proc,SqlParameter[] prams)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                SqlCommand sqlCommand = new SqlCommand(Proc, connection);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlCommand.Parameters.AddRange(prams);
+                connection.Open();
+                SqlDataAdapter sqlData = new SqlDataAdapter(sqlCommand);
+                sqlData.Fill(dt);
+                connection.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return dt;
+        }
+    }
+
+}
